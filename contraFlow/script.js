06abb05ae3943e4c8b72bc3eb738bef2ef63c416
heurrey <reymhndra10@gmail.com>
2023-09-30 17:28:14 +0700

    first commit

---
+++ contraFlow/script.js
@@ -0,0 +1,12 @@
+// alert("Mulai");
+// for(var i = 0; i < 5; i++){
+//     alert("Hello World")
+// }
+// alert("Selesai");
+
+var angka = prompt("Masukan Angka :");
+if ( angka % 2 === 0){
+    alert(angka + "adalah bilangan GENAP");
+}else{
+    alert(angka + "adalah bilangan GANJIL")
+}
---
+++ popUpBoxJavaScript/script.js
@@ -0,0 +1,25 @@
+// alert('Hello');
+// alert('nama');
+// alert('saya');
+// alert('Rey Mahendra');
+
+// var nama = prompt('Masukan Nama');
+// alert ('nama saya' + nama);
+
+// var tes = confirm('Kamu Yakin');
+// // alert(tes);
+// if (tes === true) {
+//     alert('user menekan OK!')
+// }else{
+//     alert('user menekan CANCEL!')
+// }
+
+alert("selamat datang..");
+var lagi = true;
+while(lagi === true){
+    var nama = prompt('Masukan Nama');
+    alert('Halo' + nama);
+
+    lagi = confirm('coba lagi?');
+}
+alert("terima kasih..");
